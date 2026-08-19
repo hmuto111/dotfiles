c70ea6d922cac863e6fe87ecf501ff796262ebe8
hmuto111 <h.muto111@gmail.com>
2026-08-19 22:57:57 +0900

    Neovim設定の更新とjj設定の追加

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -91,6 +91,11 @@ return {
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
       },
+      i = {
+        -- インサートモードを抜けるキーバインド
+        ["jj"] = { "<Esc>", desc = "Escape to Normal mode" },
+        ["jk"] = { "<Esc>", desc = "Escape to Normal mode" },
+      },
     },
   },
 }
